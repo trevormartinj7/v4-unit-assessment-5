b1a976cc40ab476a838fd5dac057715f246fca57
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-12-15 13:04:40 -0700

    Removed upvotes

--- db/post/create_post.sql
+++ db/post/create_post.sql
@@ -1,2 +1,2 @@
-insert into helo_posts (author_id, title, img, content, date_created, upvotes)
-values ($1, $2, $3, $4, $5, 0);
+insert into helo_posts (author_id, title, img, content, date_created)
+values ($1, $2, $3, $4, $5);
--- db/post/read_all_posts.sql
+++ db/post/read_all_posts.sql
@@ -1,3 +1,3 @@
-select p.id as post_id, title, content, img, profile_pic, username as author_username, date_created, upvotes from helo_posts p
+select p.id as post_id, title, content, img, profile_pic, username as author_username, date_created from helo_posts p
 join helo_users u on u.id = p.author_id
 order by date_created desc;
